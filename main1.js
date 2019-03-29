e511749ed1270e9194f8d4f6d84e65779fd0dbbd
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-03-29 16:24:25 +0100

    29mars

--- main1.js
+++ main1.js
@@ -102,17 +102,20 @@ function keyConverter(e) {
         let newX;
         let newXx;
         let newYy;
+        let newPXx;
+        let newPYy;
 
         let newCY;
         let newCX;
         let newCXx;
         let newCYy;
+        let check=true;
 
         newY = player.yRow + moveY;//flyttar i yaxel
         newX = player.xCell + moveX;//flyttar i x axel
 
-        newYy = player.yRow + moveY + moveY;//flyttar i yaxel
-        newXx = player.xCell + moveX + moveX;//flyttar i x axel
+        newYy = player.yRow + moveY + moveY+moveY;//flyttar i yaxel
+        newXx = player.xCell + moveX + moveX+moveX;//flyttar i x axel
 
         newCY = Crate.yRow + moveY;//flyttar i yaxel
         newCX = Crate.xCell + moveX;//flyttar i x axel
@@ -120,45 +123,61 @@ function keyConverter(e) {
         newYy = player.yRow + moveY + moveY;//flyttar i yaxel
         newXx = player.xCell + moveX + moveX;//flyttar i x axel
 
+        newPYy =Crate.yRow + moveY + moveY;//flyttar i yaxel
+        newPXx = Crate.xCell + moveX + moveX;//flyttar i x axel
         let MovePlayer = document.getElementById("y" + player.yRow + "x" + player.xCell);//constant playerElement get y och x axel
-
+        let MoveCarte=document.getElementById("y" + Crate.yRow +newY +"x" + Crate.xCell+newX);
         let Movenext = document.getElementById("y" + newY + "x" + newX);//constant playerElement get y och x axel
 
 
         let Movenextnext = document.getElementById("y" + newYy + "x" + newXx);//constant playerElement get y och x axel
-
-
+        let PlayerMovenextnext = document.getElementById("y" + moveY+moveY + "x" + moveX+moveX);//constant playerElement get y och x axel
         
-        if (Movenext.classList.contains("Floor") || Movenext.classList.contains("Goal")&& !Movenextnext.classList.contains("Crate")) {
+        
+       
+        if ((Movenext.classList.contains("Floor") || Movenext.classList.contains("Goal")) && !Movenext.classList.contains("Carte"))  {
+          
+            {
+                if (Movenext.contains("Carte")&&(!Movenextnext.contains("Crate"))) {
             MovePlayer.classList.remove("Player");
             Movenext.classList.add("Player");
 
             player.xCell = newX;
             player.yRow = newY;
+                }
 
-            if(!Movenextnext.classList.contains("wall"))
+            if(!Movenext.classList.contains("wall") )
             {
-            if(Movenext.classList.contains("Crate"))
-                       {
-            if (!Movenextnext.classList.contains("Crate") ) {
                
+           
+                console.log("innan if")
+                  if (Movenextnext.classList.contains("Crate")) { {    
+                    console.log("If början")
+           {
+               console.log("Lägga till ta bort Carte")
                 {
           
                 console.log("move Carte");
-                Movenext.classList.remove("Crate");
+                MoveCarte.classList.remove("Crate");
+                Movenext.classList.add("Carte");
 
-                Movenextnext.classList.add("Crate")
+               // Movenextnext.classList.add("Crate")
                 }
+            }
 
             }
+        }
       
         }
     }
     
+}
+
+}
     }
   
-    }
+    
 
-    }
+    
 
 DrawMap();
